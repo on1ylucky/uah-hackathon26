13fb945ca4df71fde36864d1d44ef4445af8f50b
Emi <64435252+on1ylucky@users.noreply.github.com>
2026-03-22 09:06:21 -0500

    Add color extension for theme customization

---
+++ Theme.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+extension Color {
+    static let primaryBlue = Color(red: 0.35, green: 0.45, blue: 0.95)
+    static let accentBlue = Color(red: 0.55, green: 0.65, blue: 1.0)
+    static let successGreen = Color(red: 0.15, green: 0.80, blue: 0.45)
+
+    static let appBackground = Color(red: 0.96, green: 0.97, blue: 1.0)
+    static let cardBackground = Color.white
+}
